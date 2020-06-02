009934d2e2d901b1632ae4ee35a0bb0d9eaa343e
Don <donshius@inbox.lt>
2020-06-02 18:24:48 +0100

    cleanup boilerplate, add uikit, add router

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="uk-width-1-3 uk-margin-auto uk-margin-top uk-card uk-card-default uk-card-body">
+            <div className="uk-card-title">Your tasks</div>
+        </div>
+    )
+}
